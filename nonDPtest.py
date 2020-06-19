19ae284621da3cea36cd60a7765286b0c318e2e9
Adithi Satish <45226748+adithisatish@users.noreply.github.com>
2020-06-19 12:58:18 +0530

    basic test script

---
+++ nonDPtest.py
@@ -0,0 +1,21 @@
+import psycopg2 as pg 
+import time
+import sys
+import pandas as pd
+
+try:
+#connections to the database
+    connection = pg.connect(user = 'postgres', password = "dbmssem4", host = "127.0.0.1",port = "5432",database = "uber") ##IMPORTANT: change password to 'password' before running in the lab system
+    cursor = connection.cursor()
+    #sample query
+    query = "SELECT AVG(total_distance) FROM completeride"
+    #time
+    start = time.time()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    print("Result : ",result[0][0])
+    end = time.time()
+    print("Time: ",(end-start)*1000, "ms")
+
+except Exception as e:
+    print("Unable to connect to PostgreSQL server {e}")
