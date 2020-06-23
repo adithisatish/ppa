5147bb341777019544775f4e3eca9fe87d954831
manahshetty <56164920+manahshetty@users.noreply.github.com>
2020-06-23 13:14:12 +0530

    Update nonDPtest.py

--- nonDPtest.py
+++ nonDPtest.py
@@ -22,7 +22,7 @@ try:
     for i in queries:
         testing_time = []
         result = 0
-        for j in range(10):
+        for j in range(56):
             start = time.time()
             cursor.execute(i)
             result = cursor.fetchall()
