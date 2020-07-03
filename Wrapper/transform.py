a7ff79f3c6abe0ccd509ac68cd85307784b7bd6b
Adithi Satish <45226748+adithisatish@users.noreply.github.com>
2020-07-03 22:45:21 +0530

    made changes to transform

--- Wrapper/transform.py
+++ Wrapper/transform.py
@@ -49,13 +49,20 @@ def winsorized(query,dtype):
 			return lst[0]+"anon_"+dtype+"("+lst[1]
 
 		att = lst[1].split(")")
-		table = (att[1]).lower().split("from ")[1]
+		#print(att)
+		temp = (att[1]).lower().split("from ",1)[1]
+		table = temp
+		if "(" in temp and "group by" in temp:
+			table = (temp.split("("))[0].split(" group by")[0]
+		elif "group by" in temp:
+			table = temp.split(" group by")[0]
+		#elif 
+		print(table)
 
 		lowerbound = "select percentile_cont(0.15) within group(order by " + att[0] + ") from " + table
 		upperbound = "select percentile_cont(0.85) within group(order by " + att[0] + ") from " + table
 
-		tquery = lst[0] + "anon_" + dtype + "_with_bounds(" + att[0] + ",(" + lowerbound + "),(" + upperbound + ")) from " + table
+		tquery = lst[0] + "anon_" + dtype + "_with_bounds(" + att[0] + ",(" + lowerbound + "),(" + upperbound + ")) from " + temp
 		return tquery
 
 	return query
-
